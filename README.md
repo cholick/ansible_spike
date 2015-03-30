4d6f3bdf730c99ce2e714acceeedbfb2d4e48667
Matt Cholick <cholick@gmail.com>
2015-03-29 19:40:41 -0700

    Add test script

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
+### Setup
 
+Prerequisites
+* [Bundler](http://bundler.io/)
+* [Vagrant](https://www.vagrantup.com/)
+* [Ansible](http://www.ansible.com/home)
+
+```
+bundle install
+```
+
+### Testing
+
+While developing, from the test folder run
+```
+test.sh
+```
+
+Before committing, do a fine test from a clean vm
+```
+test.sh --full
+```
+
+### References
+
+* https://github.com/fdavis/ansible-best-practices
 * http://www.geedew.com/setting-up-ansible-for-multiple-environment-deployments/
 * http://blog.cliffano.com/2014/04/06/human-readable-ansible-playbook-log-output-using-callback-plugin/
 * https://servercheck.in/blog/testing-ansible-roles-travis-ci-github
---
+++ test/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+red='\033[1;31m'
+green='\033[1;32m'
+clear='\033[0m'
+
+function usage() {
+  echo "usage:
+    $0 [--full] [--help]
+    Flags:
+      --full    Start from a fresh vagrant box & test idempotence
+      --help    This menu
+"
+  exit 0
+}
+
+if [ "$1" == "--help" ]; then
+  usage;
+fi
+
+if [ "$1" == "--full" ]; then
+  vagrant destroy --force
+fi
+
+vagrant up
+
+ansible-playbook -i ../environments/dev/inventory ../server.yml
+
+if [ "$1" == "--full" ]; then
+    ansible-playbook -i hosts ../server.yml \
+        | grep -qE "changed=0\s+unreachable=0" \
+        && (echo -e "Idempotence test: ${green}pass${clear}" && exit 0) \
+        || (echo -e "Idempotence test: ${red}fail${clear}" && exit 1)
+fi
+
+rake
